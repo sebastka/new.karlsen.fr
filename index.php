07ff9cc63671ed7087ac0e50163be85ed40588d7
Sebastian Karlsen <sebastian@karlsen.fr>
2021-06-26 19:35:53 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+
+		<title>karlsen.fr</title>
+		<meta name="description" content="Sebastian Karlsen's portfolio">
+		<meta name="keywords" content="karlsen, sebastian, portfolio">
+		<meta name="author" content="Sebastian Karlsen">
+
+		<link rel="stylesheet" href="style.css">
+	</head>
+
+	<body>
+		<table>
+			<tr>
+				<td class="username">
+					<p>sebastian</p>
+				</td>
+				
+				<td class="default">
+					<p>@</p>
+				</td>
+				
+				<td class="hostname">
+					<pre><?php require_once('karlsen.fr.ascii'); ?></pre>
+				</td>
+
+				<td class="default">
+					<p>:</p>
+				</td>
+
+				<td class="dir">
+					<p>~</p>
+				</td>
+			</tr>
+
+			<tr class="links">
+				<td colspan="5">
+					<a href="https://github.com/sebastka" title="Github" target="_blank"><img src="github.svg" alt="Github" class="invert"></a>
+					<a href="https://www.linkedin.com/in/sebastka/" title="LinkedIn" target="_blank"><img src="linkedin.svg" alt="LinkedIn" class="invert"></a>
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
