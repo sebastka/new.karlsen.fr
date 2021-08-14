fb7b9c65af411c65c66011a0edeaef2950760baa
Sebastian Karlsen <sebastian@karlsen.fr>
2021-08-14 14:34:28 +0200

    Obfuscate e-mail

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 				<td colspan="5">
 					<a href="https://github.com/sebastka" title="Github" target="_blank"><img src="github.svg" alt="Github" class="invert"></a>
 					<a href="https://www.linkedin.com/in/sebastka/" title="LinkedIn" target="_blank"><img src="linkedin.svg" alt="LinkedIn" class="invert"></a>
-					<a href="sebastian@karlsen.fr.asc" title="PGP"><img src="key.svg" alt="PGP" class="invert"></a>
+					<a href="javascript:window.location.href=atob('<?php echo base64_encode('sebastian@karlsen.fr.asc') ?>')" title="PGP Public key" class="obfuscate"><img src="key.svg" alt="PGP" class="invert"></a>
 				</td>
 			</tr>
 		</table>
